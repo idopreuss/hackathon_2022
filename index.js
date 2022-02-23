85017cd3befabe7da96fbf09ee04841c0fed2f41
idopreuss <ido.preuss@duda.co>
2022-02-23 18:06:25 +0200

    Call figma

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const PORT = process.env.PORT || 5000
 var bodyParser = require('body-parser')
 var jsonParser = bodyParser.json()
+const axios = require('axios');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -11,8 +12,30 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('pages/index'))
-    .get('/hellp', (req, res) => res.status(200).send({ file:111 }))
+    .get('/hello', (req, res) => res.status(200).send({ file:111 }))
     .post('/api', jsonParser, (req, res) => {
+        const uri = 'https://api.figma.com/v1/files/' + req.body.fileKey + '/nodes?ids=' + req.body.nodeId;
+        console.log(uri);
+        axios.get(uri, {
+                headers: {
+                    'X-FIGMA-TOKEN': '330542-09af815a-1f76-4959-ba3e-e3f62ef22310'
+                }
+            }
+            )
+            .then(res => {
+                const figmaModel = res.data;
+                const rootNodeName = 'Desktop 1440';
+                const firstNodeKey = Object.keys(figmaModel.nodes)[0];
+                const rootNode = figmaModel.nodes[firstNodeKey].document.children.find(
+                    (child) => child.name === rootNodeName
+                );
+
+                console.log('Res:',rootNode)
+
+            })
+            .catch(err => {
+                console.log('Error: ', err.message);
+            });
         const figmaRes = {
             fileKey: req.body.fileKey,
             nodeId: req.body.nodeId
